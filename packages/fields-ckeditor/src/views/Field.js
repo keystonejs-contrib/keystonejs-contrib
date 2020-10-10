49a222578138dd2297f6b782f9ee1de73da41733
Abdelrahman Abdelrahman <abogical@gmail.com>
2020-10-11 01:51:59 +0200

    WIP: Add keystone gallery support

--- packages/fields-ckeditor/src/views/Field.js
+++ packages/fields-ckeditor/src/views/Field.js
@@ -4,11 +4,13 @@ import { FieldContainer, FieldLabel } from '@arch-ui/fields';
 
 import CKEditor from '@ckeditor/ckeditor5-react';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import DropdownView from '@ckeditor/ckeditor5-ui/src/dropdown/dropdownview';
+import imageIcon from '@ckeditor/ckeditor5-core/theme/icons/image.svg';
 
 const CKEditorField = ({ onChange, autoFocus, field, errors, value = '', isDisabled }) => {
   const htmlID = `ks-input-${field.path}`;
   const accessError = errors.find(
-    error => error instanceof Error && error.name === 'AccessDeniedError'
+    (error) => error instanceof Error && error.name === 'AccessDeniedError'
   );
 
   if (accessError) return null;
@@ -23,8 +25,43 @@ const CKEditorField = ({ onChange, autoFocus, field, errors, value = '', isDisab
           data={value}
           disabled={isDisabled}
           {...field.config.editorConfig}
-          onInit={editor => {
+          onInit={(editor) => {
             if (autoFocus) editor.editing.view.focus();
+            // editor.config.removePlugins = ['ImageUpload'];
+            const { imageList } = field.config;
+            if (imageList) {
+              editor.plugins.init([
+                class KeystoneImage extends Plugin {
+                  init() {
+                    const editor = this.editor;
+                    editor.ui.componentFactory.add('keystoneImage', (locale) => {
+                      const view = DropdownView(locale);
+
+                      view.set({
+                        label: 'Insert image',
+                        icon: imageIcon,
+                        tooltip: true,
+                      });
+
+                      view.on('execute', () => {
+                        const imageURL = prompt('Image URL');
+                        editor.model.change((writer) => {
+                          editor.model.insertContent(
+                            writer.createElement('image', {
+                              src: imageUrl,
+                            }),
+                            editor.model.document.selection
+                          );
+                        });
+                      });
+
+                      return view;
+                    });
+                    editor.config.define('toolbar', 'keystoneImage');
+                  }
+                },
+              ]);
+            }
             if (field.config.editorConfig && field.config.editorConfig.onInit)
               field.config.editorConfig.onInit(editor);
           }}
